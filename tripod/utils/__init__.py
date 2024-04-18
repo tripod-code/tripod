bd06a5e1f771ae17e4fb9738e3462d28ccccd474
Til Birnstiel <birnstiel@me.com>
2024-04-18 17:12:44 +0200

    added new python to meson

--- tripod/utils/__init__.py
+++ tripod/utils/__init__.py
@@ -1,7 +1,7 @@
 '''Package containing utility classes and functions used in the simulation.'''
 
 from simframe.io.writers import hdf5writer
-from .size_distribution import get_size_distribution
+from tripod.utils.size_distribution import get_size_distribution
 
-__all__ = ["hdf5writer", "get_size_distribution"]
+__all__ = ["hdf5writer"]
 __version__ = None
