09fef86c136f1c7d76673499a85071d87ca5a071
Til Birnstiel <birnstiel@me.com>
2024-04-18 17:06:17 +0200

    fixed import of get_size_distribution

--- tripod/utils/__init__.py
+++ tripod/utils/__init__.py
@@ -1,7 +1,7 @@
 '''Package containing utility classes and functions used in the simulation.'''
 
 from simframe.io.writers import hdf5writer
-from .size_distribution import size_distribution
+from .size_distribution import get_size_distribution
 
-__all__ = ["hdf5writer", "size_distribution"]
+__all__ = ["hdf5writer", "get_size_distribution"]
 __version__ = None
