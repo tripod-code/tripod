cb99a0e65a71c2c751732dff9d71b56d629b532d
Nicolas <n.kaufmannlmu.de>
2026-02-24 10:37:18 +0100

    fixed import for new function

--- tripodpy/utils/__init__.py
+++ tripodpy/utils/__init__.py
@@ -3,9 +3,11 @@
 from tripodpy.utils.read_data import read_data
 from tripodpy.utils.size_distribution import get_size_distribution
 from tripodpy.utils.size_distribution import get_q
+from tripodpy.utils.size_distribution import sim_size_distribution
 
 __all__ = [
     "get_size_distribution",
+    "sim_size_distribution",
     "read_data",
     "get_q",
 ]
